10d00a4454488c64d28a5910019936831ad2aa81
Neda Vaheb <n.vaheb.nv@gmail.com>
2026-09-07 16:44:54 +0330

    chore:update package and sing in files

---
+++ lib/auth/session.ts
@@ -0,0 +1,23 @@
+
+import { SignJWT, jwtVerify } from "jose"
+const secret = new TextEncoder().encode(
+  process.env.AUTH_SECRET
+);
+
+export async function createToken(userId: string) {
+  return new SignJWT({ userId })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("7d")
+    .sign(secret);
+}
+
+export async function verifyToken(token: string) {
+  try {
+    const { payload } = await jwtVerify(token, secret);
+
+    return payload;
+  } catch {
+    return null;
+  }
+}
